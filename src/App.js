1ac74d179de65a0323b838aa2ac0617797a9c60c
ZSchmitt <zaiane.schmitt@gmail.com>
2022-08-10 17:19:12 +0200

    feat: base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Error404 from "./pages/Error404";
+import Home from "./pages/Home";
+import Housing from "./pages/Housing";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/housing">
+          <Route path=":housingId" element={<Housing />} />
+        </Route>
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
