f663369aea76ac7b75a3ad5daecef97c052e4dfd
ZSchmitt <zaiane.schmitt@gmail.com>
2022-08-18 16:05:49 +0200

    refactor: background img

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,12 +3,12 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="banner">
-      <div className="banner-container-img">
-        <img
+      <div className="banner-container-img banner-container-img--xs">
+        {/*  <img
           src={`${process.env.PUBLIC_URL}/banner1.png`}
-          alt="Fond bannière"
+          alt="Fond bannière nature"
           className="banner__img"
-        />
+        /> */}
       </div>
       <h1 className="banner__txt">Chez vous, partout et ailleurs</h1>
     </div>
