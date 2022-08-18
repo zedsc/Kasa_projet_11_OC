753141d0bbf63968f355362b0b1440eccb092552
ZSchmitt <zaiane.schmitt@gmail.com>
2022-08-18 16:04:48 +0200

    feat: add tags + owner + collapse components

--- src/pages/Housing.js
+++ src/pages/Housing.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import CollapseBox from "../components/CollapseBox";
 import Footer from "../components/Footer";
 import Gallery from "../components/Gallery";
 import Header from "../components/Header";
+import Owner from "../components/Owner";
+import Tag from "../components/Tag";
 
 const Housing = () => {
   const location = useLocation();
@@ -15,27 +18,33 @@ const Housing = () => {
         <Header />
         <section className="gallery-container">
           <h2 className="sr-only">Gallerie d'images</h2>
-          {/* <div>{housing.pictures[0]}</div>
-        <div>{housing.pictures[0 + 2]}</div>
-        <div>
-          {housing.pictures[0 + 5]
-            ? housing.pictures[0 + 5]
-            : housing.pictures[0]}
-        </div> */}
           <Gallery housing={housing} />
         </section>
         <section className="housing-presentation">
           <h1 className="housing-title">{housing.title}</h1>
           <h2 className="housing-location">{housing.location}</h2>
+          <div className="tags">
+            {housing.tags.map((tag) => (
+              <Tag key={housing.id + "-" + tag} tag={tag} />
+            ))}
+          </div>
+
+          <Owner owner={housing.host} rating={housing.rating} />
         </section>
         <section className="housing-details">
           <h2 className="sr-only">Description et équipements</h2>
-          <div className="housing-description">
-            <p>{housing.description}</p>
-          </div>
-          <div className="housing-equipments">
-            <p>{housing.equipments}</p>
-          </div>
+          <CollapseBox
+            id="1"
+            title="Description"
+            content={housing.description}
+          />
+          <CollapseBox
+            id="2"
+            title="Equipements"
+            content={housing.equipments.map((kit) => (
+              <span className="housing-desc__kit">{kit}</span>
+            ))}
+          />
         </section>
       </div>
       <div className="footer-container">
