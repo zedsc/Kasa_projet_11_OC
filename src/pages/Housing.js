f3cf2892db6539a8b70d3400a2a42032efb6287e
ZaianeSchmitt <zaiane.schmitt@gmail.com>
2022-08-24 21:27:13 +0200

    feat: get data from id

--- src/pages/Housing.js
+++ src/pages/Housing.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation, useParams } from "react-router-dom";
 import CollapseBox from "../components/CollapseBox";
 import Footer from "../components/Footer";
 import Gallery from "../components/Gallery";
@@ -8,53 +9,104 @@ import Owner from "../components/Owner";
 import Tag from "../components/Tag";
 
 const Housing = () => {
-  const location = useLocation();
-  const { housing } = location.state;
-  console.log(housing);
+  const userId = useParams();
+  const [data, setData] = useState({});
+  const housing = { ...data[0] };
 
-  return (
-    <div>
-      <div className="main-wrapper">
-        <Header />
-        <section className="gallery-container">
+  //const dataHousing = data;
+  //const housing = dataHousing[0];
+
+  /* const getHousing = async () => {
+    return await axios.get("/logements.json").then((result) =>
+      result.data.filter(function (housing) {
+        return housing.id === userId.housingId;
+      })
+    );
+  }; */
+
+  //console.log(getHousing);
+
+  useEffect(() => {
+    axios
+      .get("/logements.json")
+      .then((result) =>
+        result.data.filter(function (object) {
+          return object.id === userId.housingId;
+        })
+      )
+      .then((filteredData) => setData(filteredData));
+  }, []);
+
+  /*  useEffect(() => {
+    const getData = async () => {
+      const result = await axios("/logements.json");
+      const filterData = result.data.filter(function (housing) {
+        return housing.id === userId.housingId;
+      });
+      setData(filterData);
+    };
+    getData();
+  }, [data, userId]); */
+
+  //const location = useLocation();
+  //const { housing } = location.state;
+  //console.log(housing);
+  console.log(userId.housingId);
+  console.log(housing);
+  if (data === {}) {
+    return "Chargement...";
+  } else {
+    return (
+      <div>
+        <div className="main-wrapper">
+          <Header />
+          <div className="TEST">
+            {housing.id}
+            {housing.title}
+            {housing.equipments}
+            {housing.tags}
+            {housing.pictures}
+          </div>
+          {/*    <section className="gallery-container">
           <h2 className="sr-only">Gallerie d'images</h2>
           <Gallery housing={housing} />
-        </section>
-        <section className="housing-presentation">
+        </section> */}
+          {/* <section className="housing-presentation">
           <div className="housing-title-loc-tags-container">
             <h1 className="housing-title">{housing.title}</h1>
             <h2 className="housing-location">{housing.location}</h2>
-            <div className="tags">
+             <div className="tags">
               {housing.tags.map((tag) => (
                 <Tag key={housing.id + "-" + tag} tag={tag} />
               ))}
-            </div>
+            </div> 
           </div>
           <Owner owner={housing.host} rating={housing.rating} />
-        </section>
-        <section className="housing-details">
-          <h2 className="sr-only">Description et équipements</h2>
-          <CollapseBox
-            id="1"
-            title="Description"
-            content={housing.description}
-            className="housing-desc"
-          />
-          <CollapseBox
-            id="2"
-            title="Equipements"
-            className="housing-desc"
-            content={housing.equipments.map((kit) => (
-              <span className="housing-desc__kit">{kit}</span>
-            ))}
-          />
-        </section>
+        </section>*/}
+          <section className="housing-details">
+            <h2 className="sr-only">Description et équipements</h2>
+            <CollapseBox
+              id="1"
+              title="Description"
+              content={housing.description}
+              className="housing-desc"
+            />
+            <CollapseBox
+              id="2"
+              title="Equipements"
+              className="housing-desc"
+              content={housing.equipments.map((kit) => (
+                <span className="housing-desc__kit">{kit}</span>
+              ))}
+            />
+          </section>
+        </div>
+        <div className="footer-container">
+          <Footer />
+        </div>
       </div>
-      <div className="footer-container">
-        <Footer />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Housing;
