02eacd77f437193c582daa12a7320cfe3c7d8311
ZSchmitt <zaiane.schmitt@gmail.com>
2022-09-02 18:11:31 +0200

    refactor: move BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Error404 from "./pages/Error404";
 import Home from "./pages/Home";
@@ -7,16 +7,16 @@ import Housing from "./pages/Housing";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/housing">
-          <Route path=":housingId" element={<Housing />} />
-        </Route>
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </BrowserRouter>
+    //<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/housing">
+        <Route path=":housingId" element={<Housing />} />
+      </Route>
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+    //</BrowserRouter>
   );
 };
 
